e1fd02f0e5334e3e76706a3a65c87e229557c323
Francis Langit <francis.villanueva.langit@gmail.com>
2021-07-10 18:49:10 +0800

    Create placeholder object in calculator.js

---
+++ src/calculator/calculator.js
@@ -0,0 +1,6 @@
+const calulator = {
+    add: (numberA, numberB) => {null},
+    subtract: (numberA, numberB) => {null},
+    multiply: (numberA, numberB) => {null},
+    divide: (numberA, numberB) => {null},
+}
