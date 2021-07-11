44c11c715a5e02df850980f1abb19acf8037457c
Francis Langit <francis.villanueva.langit@gmail.com>
2021-07-11 15:46:05 +0800

    Refactor caesarCipher with caesarCipherCharacter

--- src/caesarCipher/caesarCipher.js
+++ src/caesarCipher/caesarCipher.js
@@ -6,7 +6,33 @@
 
 
 /**
- * Encrypts a string using a caesar cipher.
+ * Offsets an alphabetical character using a Caesar Cipher. Returns 
+ * non-alphabetical character as is if such is passed in.
+ * 
+ * @param {char} character Character to encrypt.
+ * @param {int} key Key of cipher. Offset applied to character.
+ *  
+ * @returns Character offset using a Caesar Cipher.
+ * 
+ * @example
+ * let offsetCharacter = caesarCipherCharacter('a', 3);
+ * console.log(offsetCharacter) // Logs 'd' in console.
+ */
+const caesarCipherCharacter = (character, key) => {
+    if (character.match(/[a-z]/i)) {
+        let code = character.charCodeAt(0);
+        if (code >= 65 && code <= 90) {
+            return String.fromCharCode(((code - 65 + key) % 26) + 65);
+        } else if (code >= 97 && code <= 122) {
+            return String.fromCharCode(((code - 97 + key) % 26) + 97);
+        }
+    }
+    return character;
+}
+
+
+/**
+ * Encrypts a string using a Caesar Cipher.
  * 
  * @param {string} string String to encrypt. 
  * @param {int} key Key of cipher. Offset applied to each letter in string.
@@ -21,40 +47,17 @@
  * console.log(encryptedString); 
  */
 const caesarCipher = (string, key) => {
-    // Wrap the amount
+    // If key negative, wrap it.
     if (key < 0) {
         return caesarCipher(string, key + 26);
     }
 
-    // Make an output variable
-    var output = "";
-
-    // Go through each character
-    for (var i = 0; i < string.length; i++) {
-        // Get the character we'll be appending
-        var c = string[i];
-
-        // If it's a letter...
-        if (c.match(/[a-z]/i)) {
-            // Get its code
-            var code = string.charCodeAt(i);
-
-            // Uppercase letters
-            if (code >= 65 && code <= 90) {
-                c = String.fromCharCode(((code - 65 + key) % 26) + 65);
-            }
-
-            // Lowercase letters
-            else if (code >= 97 && code <= 122) {
-                c = String.fromCharCode(((code - 97 + key) % 26) + 97);
-            }
-        }
-
-        // Append
-        output += c;
+    // Encrypt characters in string.
+    let output = "";
+    for (let i = 0; i < string.length; i++) {
+        output += caesarCipherCharacter(string[i], key);
     }
 
-    // All done!
     return output;
 }
 
