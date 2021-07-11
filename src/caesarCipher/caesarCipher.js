a6261e6711faa84b0301052fd42106b4f01078d2
Francis Langit <francis.villanueva.langit@gmail.com>
2021-07-11 15:00:32 +0800

    Write documentation for caesarCipher function

--- src/caesarCipher/caesarCipher.js
+++ src/caesarCipher/caesarCipher.js
@@ -1,4 +1,26 @@
-const caesarCipher = (string) => {
+/**
+ * Module holding and exporting caesarCipher function.
+ * 
+ * @module
+ */
+
+
+/**
+ * Encrypts a string using a caesar cipher.
+ * 
+ * @param {string} string String to encrypt. 
+ * @param {int} key Key of cipher. Offset applied to each letter in string.
+ * 
+ * @returns String encrypted using a caesar cipher.
+ * 
+ * @example
+ * let string = "I'm the real slim shady.";
+ * let encryptedString = caesarCipher(string, 13);
+ * 
+ * // Logs "v'z gur erny fyvz funql." in console.
+ * console.log(encryptedString); 
+ */
+const caesarCipher = (string, key) => {
     return null;
 }
 
--- src/caesarCipher/caesarCipher.test.js
+++ src/caesarCipher/caesarCipher.test.js
@@ -2,11 +2,11 @@ import { caesarCipher } from './caesarCipher';
 
 
 test('Caesar cipher of string offset by 1', () => {
-    expect(caesarCipher('HELLO!')).toBe('IFMMP!');
+    expect(caesarCipher('HELLO!', 1)).toBe('IFMMP!');
 });
 
 test('Caesar cipher of alpha-numberic string offset by 13', () => {
     let input = 'The 5 quick brown foxes jump over the 10 lazy dogs.';
     let output = 'ymj 5 vznhp gwtbs ktcjx ozru tajw ymj 10 qfed itlx.';
-    expect(caesarCipher(input)).toBe(output);
+    expect(caesarCipher(input, 13)).toBe(output);
 });
