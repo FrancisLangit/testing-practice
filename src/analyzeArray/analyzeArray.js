0023d0ebd7ff267b43a25896ea908d2880da1626
Francis Langit <francis.villanueva.langit@gmail.com>
2021-07-11 16:13:00 +0800

    Write documentation for analyzeArray function

--- src/analyzeArray/analyzeArray.js
+++ src/analyzeArray/analyzeArray.js
@@ -1,3 +1,24 @@
+/**
+ * Module containing and exporting analyzeArray function.
+ * 
+ * @module
+ */
+
+
+/**
+ * Takes an array of numbers returns an object with the average, min, max, and
+ * length of numbers in such array.
+ * 
+ * @param {Array} array Array of integers to be analyzed.
+ * 
+ * @returns {Object} Object with average, min, max, and length of numbers.
+ * 
+ * @example
+ * let analyzedArray = analyzeArray([1,8,3,4,2,6]);
+ * 
+ * // Logs "{ average: 4, min: 1, max: 8, length: 6 }" in console.
+ * console.log(analyzedArray);
+ */
 const analyzeArray = (array) => {
     return null;
 }
